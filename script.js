bf8d267c0f090aca2ba6d1a3fa3d2e342959c512
abhay <jabhay2012@gmail.com.com>
2020-11-15 15:30:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+const modalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+for(let i = 0 ; i < modalBtns.length ; i++){
+    modalBtns[i].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+       
+    })
+}
+
+const closeModal = function(){
+   
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+   
+    
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown',function(evt){
+    if(evt.key === "Escape"){
+    closeModal();
+    }
+})
